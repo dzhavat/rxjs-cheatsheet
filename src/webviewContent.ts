a6650438e08a2a658374286c8ed9915824ceedcc
Dzhavat Ushev <dzhavatushev@gmail.com>
2020-05-10 11:46:07 +0200

    Add operators

--- src/webviewContent.ts
+++ src/webviewContent.ts
@@ -21,45 +21,50 @@ export function getWebviewContent(stylePath: Uri, imagesPath: Uri) {
 
 				<h2 class="center">Creation Functions</h2>
 
-				<div class="item">
+				<div class="row">
 					<h2>concat (<a href="https://rxjs.dev/api/index/function/concat" target="_blank">Docs</a>)</h2>
 
+					<h3>How to use</h3>
 					<pre><code>import { concat } from 'rxjs';</code></pre>
 
 					<h3>Marble diagram</h3>
 					<img src="${imagesPath}/concat.png" alt="Marble diagram" />
 				</div>
 
-				<div class="item">
+				<div class="row">
 					<h2>from (<a href="https://rxjs.dev/api/index/function/from" target="_blank">Docs</a>)</h2>
 
+					<h3>How to use</h3>
 					<pre><code>import { from } from 'rxjs';</code></pre>
 
 					<h3>Marble diagram</h3>
 					<img src="${imagesPath}/from.png" alt="Marble diagram" />
 				</div>
 
-				<div class="item">
+				<div class="row">
 					<h2>fromEvent (<a href="https://rxjs.dev/api/index/function/fromEvent" target="_blank">Docs</a>)</h2>
 
+					<h3>How to use</h3>
 					<pre><code>import { fromEvent } from 'rxjs';</code></pre>
 
 					<h3>Marble diagram</h3>
 					<img src="${imagesPath}/fromEvent.png" alt="Marble diagram" />
 				</div>
 
-				<div class="item">
+				<div class="row">
 					<h2>merge (<a href="https://rxjs.dev/api/index/function/merge" target="_blank">Docs</a>)</h2>
 
+					<h3>How to use</h3>
 					<pre><code>import { merge } from 'rxjs';</code></pre>
 
 					<h3>Marble diagram</h3>
 					<img src="${imagesPath}/merge.png" alt="Marble diagram" />
 				</div>
 
-				<div class="item">
+				<div class="row">
 					<h2>of (<a href="https://rxjs.dev/api/index/function/of" target="_blank">Docs</a>)</h2>
 
+					<h3>How to use</h3>
 					<pre><code>import { of } from 'rxjs';</code></pre>
 
 					<h3>Marble diagram</h3>
@@ -67,6 +72,36 @@ export function getWebviewContent(stylePath: Uri, imagesPath: Uri) {
 				</div>
 
 				<h2 class="center">Operators</h2>
+
+				<div class="row">
+					<h2>filter (<a href="https://rxjs.dev/api/operators/filter" target="_blank">Docs</a>)</h2>
+
+					<h3>How to use</h3>
+					<pre><code>import { filter } from 'rxjs/operators';</code></pre>
+
+					<h3>Marble diagram</h3>
+					<img src="${imagesPath}/filter.png" alt="Marble diagram" />
+				</div>
+
+				<div class="row">
+					<h2>map (<a href="https://rxjs.dev/api/operators/map" target="_blank">Docs</a>)</h2>
+
+					<h3>How to use</h3>
+					<pre><code>import { map } from 'rxjs/operators';</code></pre>
+
+					<h3>Marble diagram</h3>
+					<img src="${imagesPath}/map.png" alt="Marble diagram" />
+				</div>
+
+				<div class="row">
+					<h2>tap (<a href="https://rxjs.dev/api/operators/tap" target="_blank">Docs</a>)</h2>
+
+					<h3>How to use</h3>
+					<pre><code>import { tap } from 'rxjs/operators';</code></pre>
+
+					<h3>Marble diagram</h3>
+					<img src="${imagesPath}/tap.png" alt="Marble diagram" />
+				</div>
 			</div>
 		</main>	
 	</body>
